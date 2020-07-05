0f33290faa1d9d822a71b49a6b5895b940d1c0cf
seasons986 <annalin12345@gmail.com>
2020-07-04 19:43:33 -0700

    Oooh Yayoi Kusama

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ const sketch = () => {
                 const u = count <= 1 ? 0.5 : x / (count - 1);
                 const v = count <= 1 ? 0.5 : y / (count - 1);
                 points.push({
-                    radius: random.value() * 0.04,
+                    radius: random.value() * 0.01,
                     position: [u, v]
                 });
             }
@@ -41,9 +41,11 @@ const sketch = () => {
 
             context.beginPath();
             context.arc(x, y, radius * width, 0, Math.PI * 2, false);
-            context.strokeStyle = 'black';
-            context.lineWidth = 20;
-            context.stroke();
+            // context.strokeStyle = 'black';
+            // context.lineWidth = 20;
+            // context.stroke();
+            context.fillStyle = 'red';
+            context.fill();
         });
     };
 };
