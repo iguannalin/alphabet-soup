45529ff674246d977b9d27d6a3712d4007abc740
seasons986 <annalin12345@gmail.com>
2020-07-04 19:22:20 -0700

    Learning how to draw a circle

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-    dimensions: [2048, 2048]
+    dimensions: 'A4',
+    units: 'cm',
+    pixelsPerInch: 300
 };
 
 const sketch = () => {
     return ({context, width, height}) => {
+        console.log(width, height);
         context.fillStyle = 'orange';
         context.fillRect(0, 0, width, height);
 
         context.beginPath();
-        context.arc(width / 2, height / 2, 300, 0, Math.PI * 2, false);
+        context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
         context.fillStyle = 'red';
         context.fill();
-        context.lineWidth = 20;
+        context.lineWidth = width * 0.05;
+        context.strokeStyle = 'blue';
         context.stroke();
     };
 };
