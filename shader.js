7e8c908659a478d8f9912c4816da7f497022f7f6
seasons986 <annalin12345@gmail.com>
2020-08-15 16:02:06 -0700

    Cool circle

--- shader.js
+++ shader.js
@@ -17,19 +17,26 @@ const frag = glsl(`
   varying vec2 vUv;
   
   #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
 
   void main () {
     // vec3 colorA = vec3(1.0,0.0,0.0);
     // vec3 colorB = vec3(0.0,0.0,1.0);
     //
-    // vec2 center = vUv - 0.5;
-    // center.x *= aspect;
-    // float dist = length(center);
-    // float alpha = smoothstep(0.75, 0.5, dist);
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
+    float dist = length(center);
+    float alpha = smoothstep(0.25, 0.2475, dist);
     //
     // vec3 color = mix(colorA, colorB, vUv.x + vUv.y);
-    float n = noise(vec3(vUv.xy, time));
-    gl_FragColor = vec4(vec3(n), 1.0);
+    float n = noise(vec3(center* 0.5, time * 1.0));
+    
+    vec3 color = hsl2rgb(
+        0.6 + n * 0.2,
+        0.5,
+        0.5
+    );
+    gl_FragColor = vec4(color, alpha);
   }
 `);
 
@@ -46,7 +53,7 @@ const sketch = ({gl}) => {
         uniforms: {
             // Expose props from canvas-sketch
             time: ({time}) => time,
-            aspect: ({ width, height }) => width/height
+            aspect: ({width, height}) => width / height
         }
     });
 };
